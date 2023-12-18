9a539f1d95ca984fb58ba6dfa8b67cb2f5f515ff
Magda-J <magdalena.jakonska@gmail.com>
2023-12-18 14:00:40 +0000

    Initial Version

---
+++ index.js
@@ -0,0 +1,80 @@
+const textInput = document.querySelector("#textInput");
+const textResult = document.querySelector("#textResult");
+const encodeButton = document.querySelector("#encode");
+const decodeButton = document.querySelector("#decode");
+const keyInput = document.querySelector("#key");
+
+encodeButton.addEventListener("click", () => {
+  let keyValue = Number(keyInput.value);
+  if (keyValue < 0) {
+    textResult.value = "Please enter a positive number";
+  } else {
+    shift(keyValue);
+    textResult.value = encodeFunc(textInput.value);
+  }
+});
+
+decodeButton.addEventListener("click", () => {
+  let keyValue = Number(keyInput.value);
+  if (keyValue < 0) {
+    textResult.value = "Please enter a positive number";
+  } else {
+    shift(keyValue);
+    textResult.value = decodeFunc(textInput.value);
+  }
+});
+
+// Start of the cipher code
+var alphabet = "abcdefghijklmnopqrstuvwxyz"; // Create a new variable that holds the alphabet
+var newalpha = ""; // Create another variable for a new alphabet and keep it empty
+
+// The shift function takes an integer n and creates a shifted alphabet (newalpha).
+// It uses a loop to iterate over each character in the original alphabet,
+// calculates the new index after a circular shift by n, and builds the shifted alphabet.
+
+function shift(n) {
+  newalpha = ""; // Reset newalpha before shifting
+  for (let i = 0; i < alphabet.length; i++) {
+    let offset = (i + n) % alphabet.length;
+    newalpha += alphabet[offset];
+  }
+}
+
+// The encodeFunc function takes a message and encodes it using the shifted alphabet (newalpha). 
+// It converts each character to lowercase, checks if it's in the original alphabet, finds its index, 
+// and replaces it with the corresponding character from the shifted alphabet. Non-alphabetic characters remain unchanged.
+
+function encodeFunc(message) {
+  let result = "";
+  message = message.toLowerCase();
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+    if (alphabet.includes(char)) {
+      let index = alphabet.indexOf(char);
+      result += newalpha[index];
+    } else {
+      // If the character is not alphabetic, keep it unchanged
+      result += char;
+    }
+  }
+  return result;
+}
+
+// The decodeFunc function takes an encoded message and decodes it by finding the index of each character in the shifted alphabet (newalpha) 
+// and replacing it with the corresponding character from the original alphabet. Non-alphabetic characters remain unchanged.
+
+function decodeFunc(message) {
+  let result = "";
+  message = message.toLowerCase();
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+    if (alphabet.includes(char)) {
+      let index = newalpha.indexOf(char);
+      result += alphabet[index];
+    } else {
+      // If the character is not alphabetic, keep it unchanged
+      result += char;
+    }
+  }
+  return result;
+}
